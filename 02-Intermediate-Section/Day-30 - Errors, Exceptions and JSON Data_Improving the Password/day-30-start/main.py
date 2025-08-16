8e3c8afabbd913824ec248665f900a9f2b0eb0f1
Afira Arif <afiraarif38@gmail.com>
2025-08-16 10:58:40 +0500

    complete day 30

---
+++ 02-Intermediate-Section/Day-30 - Errors, Exceptions and JSON Data_Improving the Password/day-30-start/main.py
@@ -0,0 +1,15 @@
+#FileNotFound
+# with open("a_file.txt") as file:
+#     file.read()
+
+# KeyError
+# a_dictionary = {"key": "value"}
+# value = a_dictionary["non_existent_key"]
+
+#IndexError
+# fruits = ["Apple", "Banana", "Pear"]
+# fruit = fruits[3]
+
+#TypeError
+# text = "abc"
+# print(text + 5)
