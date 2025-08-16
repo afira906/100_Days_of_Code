9fcc910df0fd58122069e0a4b9496d3673df0e25
Afira Arif <afiraarif38@gmail.com>
2025-08-16 10:52:18 +0500

    complete day 24

---
+++ 02-Intermediate-Section/Day-24 - Files, Directories and Paths/day-24-start/main.py
@@ -0,0 +1,15 @@
+# file = open("my_file.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+## If file is in desktop folder
+# with open("../../../OneDrive/Desktop/my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# with open("my_file.txt", mode="a") as file:
+#     file.write("I am 19 years old.")
+
+# with open("new_file.txt", mode="w") as file:
+#      file.write("I will become more disciplined.")
