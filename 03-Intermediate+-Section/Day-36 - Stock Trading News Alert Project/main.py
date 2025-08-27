897d6397c53d0f70d66917a2af485b3a12dd279d
Afira Arif <afiraarif38@gmail.com>
2025-08-27 08:12:33 +0500

    complete day 36

---
+++ 03-Intermediate+-Section/Day-36 - Stock Trading News Alert Project/main.py
@@ -0,0 +1,75 @@
+import requests
+from twilio.rest import Client
+import os
+from dotenv import load_dotenv
+
+# Load variables from .env
+load_dotenv()
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY = os.getenv("STOCK_API_KEY")
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+TWILIO_SID = os.getenv("TWILIO_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_API_KEY
+}
+
+# Get yesterday's closing stock price.
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+response.raise_for_status()
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+print(yesterday_closing_price)
+
+# Get the day before yesterday's closing stock price
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
+print(day_before_yesterday_closing_price)
+
+# Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "📈"
+else:
+    up_down = "📉"
+
+# Percentage difference in price between closing price yesterday and closing price the day before yesterday.
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
+print(diff_percent)
+
+# If difference percentage is greater than 5, use the News API to get articles related to the COMPANY_NAME.
+if abs(diff_percent) > 1:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME
+    }
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+
+    # Use Python slice operator to create a list that contains the first 3 articles.
+    three_articles = articles[:3]
+
+    # Create a new list of the first 3 article's headline and description using list comprehension.
+    formatted_article = [(f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. "
+                          f"\nBrief: {article['description']}")for article in three_articles]
+
+    # Send each article as a separate message via Twilio.
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+    for article in formatted_article:
+        message = client.messages.create(
+            body=article,
+            from_="",
+            to=""
+        )
