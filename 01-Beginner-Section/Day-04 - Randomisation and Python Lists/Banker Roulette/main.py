dda889f5984bee02b72988bd6b1aecb9a9f699d6
Afira Arif <afiraarif38@gmail.com>
2025-09-15 11:35:22 +0500

    Complete Day 4

---
+++ 01-Beginner-Section/Day-04 - Randomisation and Python Lists/Banker Roulette/main.py
@@ -0,0 +1,9 @@
+import random
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+# choice1
+print(random.choice(friends))
+
+# choice2
+random_index = random.randint(0,4)
+print(friends[random_index])
