13a80e3a0196313ae4a9ff932f5557771cb1e8f3
Afira Arif <afiraarif38@gmail.com>
2025-10-02 06:44:59 +0500

    Complete Day 10

---
+++ 01-Beginner-Section/Day-10 - Functions with Outputs/Multiple Return Values/main.py
@@ -0,0 +1,25 @@
+# def format_name(f_name, l_name):
+#     if f_name == "" or l_name == "":
+#         return "You didn't provide valid inputs."
+#
+#     formated_f_name = f_name.title()
+#     formated_l_name = l_name.title()
+#     return f"{formated_f_name} {formated_l_name}"
+#
+# print(format_name(input("What is your first name?"), input("What is your last name?")))
+
+
+# Leap year
+def is_leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+print(is_leap_year(int(input(f"Enter a year: "))))
